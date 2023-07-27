358e26325a61dade7eeffd40e4ee19d21e968795
Petro Psekho <psekho.pp@gmail.com>
2023-07-27 15:35:05 +0300

    chat in progress

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import styles from "../styles/Chat.module.css";
 
 import io from "socket.io-client";
-
 const socket = io.connect("http://localhost:5000");
 
 const Chat = () => {
@@ -10,6 +10,7 @@ const Chat = () => {
 
   const [params, setParams] = useState(null);
   const [state, setState] = useState([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const searchParams = Object.fromEntries(new URLSearchParams(search));
@@ -25,7 +26,38 @@ const Chat = () => {
 
   console.log("state", state);
 
-  return <div>Chat</div>;
+  const leftRoom = () => {};
+  const handleChange = () => {};
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.header}>
+        <div className={styles.title}>{params.room}</div>
+        <div className={styles.users}> 0 users in this room</div>
+        <button className={styles.left} onClick={leftRoom}>
+          Left the room
+        </button>
+      </div>
+      <div className={styles.messages}>
+        {state.map(({ message }) => (
+          <span>{message}</span>
+        ))}
+      </div>
+      <form className={styles.form}>
+        <div className={styles.input}>
+          <input
+            type="text"
+            name="message"
+            placeholder="What do you wont to say? "
+            value={message}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
