975c363efd158830902753c0759e61306b746805
Petro Psekho <psekho.pp@gmail.com>
2023-08-08 20:01:39 +0300

    add SYSTEM_MESSAGE_TEMPLATE

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,23 +1,16 @@
-import React, { useEffect, useState } from "react";
-import styles from "../styles/Messages.module.css";
+import { useEffect, useState } from 'react';
+import styles from '../styles/Messages.module.css';
 
 const Messages = ({ messages, name, lang }) => {
   const [translatedMessages, setTranslatedMessages] = useState([]);
-  const langUser = lang;
-
-  // console.log("langUser", langUser);
-  // console.log("name", name);
-  // console.log("messages", messages);
 
   useEffect(() => {
-    // Функция для перевода сообщения
     async function translateMessage(message, lang) {
-      console.log(lang);
       try {
-        const response = await fetch("http://localhost:5000/translate", {
-          method: "POST",
+        const response = await fetch('http://localhost:5000/translate', {
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({ message, lang }),
         });
@@ -25,47 +18,40 @@ const Messages = ({ messages, name, lang }) => {
         const data = await response.json();
         return data.translatedMessage;
       } catch (error) {
-        console.error("Ошибка при переводе сообщения:", error.message);
-        return "Translation Error";
+        console.error('Error while translating a message:', error.message);
+        return 'Translation Error';
       }
     }
 
-    // При каждом обновлении сообщений, переводим каждое сообщение на язык комнаты
     const translateMessages = async () => {
       try {
         const translated = await Promise.all(
           messages.map(({ user, message }) => {
-            console.log("user", user);
-            console.log("message", message);
-
-            if (user.name === "Admin") {
+            if (user.name === 'Admin') {
               return message;
             }
             return user.name.trim().toLowerCase() === name.trim().toLowerCase()
               ? message
-              : translateMessage(message, langUser); // Вместо user.lang используем langUser
+              : translateMessage(message, lang);
           })
         );
         setTranslatedMessages(translated);
       } catch (error) {
-        console.error("Ошибка при переводе сообщения:", error.message);
-        // Если возникла ошибка при переводе, заменяем непереведенные сообщения текстом об ошибке
-        const errorMessages = messages.map(() => "Translation Error");
+        console.error('Error while translating a message:', error.message);
+
+        const errorMessages = messages.map(() => 'Translation Error');
         setTranslatedMessages(errorMessages);
       }
     };
 
     translateMessages();
-  }, [messages, name, langUser]);
+  }, [messages, name, lang]);
 
   return (
     <div className={styles.messages}>
       {messages.map(({ user }, i) => {
-        const itsMe =
-          user.name.trim().toLowerCase() === name.trim().toLowerCase();
+        const itsMe = user.name.trim().toLowerCase() === name.trim().toLowerCase();
         const className = itsMe ? styles.me : styles.user;
-
-        console.log("messages ====>", messages);
 
         return (
           <div key={i} className={`${styles.message} ${className}`}>
--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 const app = express();
-const axios = require("axios");
+const axios = require('axios');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const { API_KEY, API_URL, MODEL } = process.env;
+const { API_KEY, API_URL, MODEL, SYSTEM_MESSAGE_TEMPLATE } = process.env;
 
-const route = require("./route");
+const route = require('./route');
 
-const { addUser, findUser, getRoomUsers, removeUser } = require("./users");
+const { addUser, findUser, getRoomUsers, removeUser } = require('./users');
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: '*' }));
 app.use(route);
 app.use(express.json());
 
@@ -21,12 +21,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: '*',
+    methods: ['GET', 'POST'],
   },
 });
 
-app.post("/translate", async (req, res) => {
+app.post('/translate', async (req, res) => {
   const { message, lang } = req.body;
   try {
     const response = await axios.post(
@@ -35,16 +35,16 @@ app.post("/translate", async (req, res) => {
         model: MODEL,
         messages: [
           {
-            role: "system",
-            content: `you're a professional polyglot translator, translate the message into ${lang}`,
+            role: 'system',
+            content: SYSTEM_MESSAGE_TEMPLATE.replace('{lang}', lang),
           },
-          { role: "user", content: message },
+          { role: 'user', content: message },
         ],
-        temperature: 0.7,
+        temperature: 0.2,
       },
       {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${API_KEY}`,
         },
       }
@@ -54,12 +54,11 @@ app.post("/translate", async (req, res) => {
     res.json({ translatedMessage });
   } catch (error) {
     console.error(`Call error API ${MODEL}:`, error.message);
-    res.status(500).json({ error: "Translation Error" });
+    res.status(500).json({ error: 'Translation Error' });
   }
 });
 
 async function translateMessage(message, lang) {
-  console.log("lang =======>", lang);
   try {
     const response = await axios.post(
       API_URL,
@@ -67,16 +66,16 @@ async function translateMessage(message, lang) {
         model: MODEL,
         messages: [
           {
-            role: "system",
-            content: `you're a professional polyglot translator, translate the message into ${lang}`,
+            role: 'system',
+            content: SYSTEM_MESSAGE_TEMPLATE.replace('{lang}', lang),
           },
-          { role: "user", content: message },
+          { role: 'user', content: message },
         ],
-        temperature: 0.7,
+        temperature: 0.2,
       },
       {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${API_KEY}`,
         },
       }
@@ -90,73 +89,60 @@ async function translateMessage(message, lang) {
   }
 }
 
-io.on("connection", (socket) => {
-  socket.on("join", ({ name, room, lang }) => {
+io.on('connection', socket => {
+  socket.on('join', ({ name, room, lang }) => {
     socket.join(room);
-
-    console.log({ name, room, lang });
 
     const { user, isExist } = addUser({ name, room, lang });
 
-    const userMessage = isExist
-      ? `${user.name}, here you go again`
-      : `Hello ${user.name}`;
+    const userMessage = isExist ? `${user.name}, here you go again` : `Hello ${user.name}`;
 
-    socket.emit("message", {
-      data: { user: { name: "Admin" }, message: userMessage },
+    socket.emit('message', {
+      data: { user: { name: 'Admin' }, message: userMessage },
     });
 
-    socket.broadcast.to(user.room).emit("message", {
-      data: { user: { name: "Admin" }, message: `${user.name} has joined` },
+    socket.broadcast.to(user.room).emit('message', {
+      data: { user: { name: 'Admin' }, message: `${user.name} has joined` },
     });
 
-    io.to(user.room).emit("room", {
+    io.to(user.room).emit('room', {
       data: { users: getRoomUsers(user.room) },
     });
   });
 
-  socket.on("sendMessage", async ({ message, params }) => {
+  socket.on('sendMessage', async ({ message, params }) => {
     const user = findUser(params);
 
-    console.log("message ===>", message);
-    console.log("user ===>", user);
-
-    // if (user) {
-    //   io.to(user.room).emit("message", { data: { user, message } });
-    // }
-
     if (user) {
-      // Здесь вызываем функцию для перевода сообщения на язык комнаты
       const translatedMessage = await translateMessage(message, user.lang);
 
-      // Отправляем переведенное сообщение обратно в комнату
-      io.to(user.room).emit("message", {
+      io.to(user.room).emit('message', {
         data: { user, message: translatedMessage },
       });
     }
   });
 
-  socket.on("leftRoom", ({ params }) => {
+  socket.on('leftRoom', ({ params }) => {
     const user = removeUser(params);
 
     if (user) {
       const { room, name } = user;
 
-      io.to(room).emit("message", {
-        data: { user: { name: "Admin" }, message: `${name} has left` },
+      io.to(room).emit('message', {
+        data: { user: { name: 'Admin' }, message: `${name} has left` },
       });
 
-      io.to(room).emit("room", {
+      io.to(room).emit('room', {
         data: { users: getRoomUsers(room) },
       });
     }
   });
 
-  io.on("disconnect", () => {
-    console.log("Disconnect");
+  io.on('disconnect', () => {
+    console.log('Disconnect');
   });
 });
 
 server.listen(5000, () => {
-  console.log("==== Server is running ====");
+  console.log('==== Server is running ====');
 });
